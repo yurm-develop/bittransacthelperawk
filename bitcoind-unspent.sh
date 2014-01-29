db236c84c9886d0e56343e6efc64f0b2baf2d671
yurm-develop <yurm.develop@gmail.com>
2014-01-29 19:51:52 +0400

    bitcoind-unspent.sh corrected (default account).

--- bitcoind-unspent.sh
+++ bitcoind-unspent.sh
@@ -10,7 +10,7 @@ bitcoind listunspent | sed -n '
 /"txid" *:/          {s/^ *"txid" *: *"\([^"]\+\)".*$/\1/;      h};
 /"vout" *:/          {s/^ *"vout" *: *\([^,]\+\),$/\1/;         H};
 /"address" *:/       {s/^ *"address" *: *"\([^"]\+\)".*$/\1/;   H};
-/"account" *:/       {s/^ *"account" *: *\("[^"]\+"\).*$/\1/;   H};
+/"account" *:/       {s/^ *"account" *: *\("[^"]*"\).*$/\1/;    H};
 /"amount" *:/        {s/^ *"amount" *: *\([^,]\+\),$/\1/;       H};
 /"confirmations" *:/ {s/^ *"confirmations" *: *\([^,]\+\)$/\1/; H; x; s/\n/\t/g; p}
-' | sed 's/^\([[:xdigit:]]\+\)\t\([[:digit:]]\+\)\t\([[:alnum:]]\+\)\t\("[^"]\+"\)\t\([[:digit:]\.]\+\)\t\([[:digit:]]\+\)/i\t\1 \2\t-\t\5\t-\t\3\t\6\t\4/' | sort -k 9
+' | sed 's/^\([[:xdigit:]]\+\)\t\([[:digit:]]\+\)\t\([[:alnum:]]\+\)\t\("[^"]*"\)\t\([[:digit:]\.]\+\)\t\([[:digit:]]\+\)$/i\t\1 \2\t-\t\5\t-\t\3\t\6\t\4/' | sort -k 9
